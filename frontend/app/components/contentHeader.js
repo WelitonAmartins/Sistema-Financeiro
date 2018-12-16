71f4d766bb08811fb39d22bc47f81fefb0f81569
Weliton Alves Martins <weliton.martins@yaman.com.br>
2018-12-16 16:37:33 -0200

    transformando header do template do dashboard em component

---
+++ frontend/app/components/contentHeader.js
@@ -0,0 +1,13 @@
+angular.module('primeiraApp').component('contentHeader', {
+    bindings: {
+        titulo: '@',
+        subtitulo: '@'
+    },
+
+    template: `
+    <section class="content-header">
+        <h1>{{ $ctrl.titulo }} <small>{{ $ctrl.subtitulo }}</small> </h1>
+    </section>
+
+    `
+})
